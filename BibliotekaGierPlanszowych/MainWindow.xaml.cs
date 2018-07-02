4ca00ad0c0357dc796f03e6c64b6caccc3e5233b
mozzyDev <tomaszmozgwa@gmail.com>
2018-07-02 12:11:09 +0200

    first commit

--- BibliotekaGierPlanszowych/MainWindow.xaml.cs
+++ BibliotekaGierPlanszowych/MainWindow.xaml.cs
@@ -15,9 +15,7 @@ using System.Windows.Shapes;
 
 namespace BibliotekaGierPlanszowych
 {
-    /// <summary>
-    /// Logika interakcji dla klasy MainWindow.xaml
-    /// </summary>
+    
     public partial class MainWindow : Window
     {
         public MainWindow()
