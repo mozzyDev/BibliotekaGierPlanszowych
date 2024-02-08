4c6eb9a9fd7300dddc322730b1854c0f6f417a99
Tomasz Mozgwa <t.mozgwa@sente.pl>
2024-02-08 09:22:34 +0100

    SignUpWindow

---
+++ BibliotekaGierPlanszowych/SignUpWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+
+namespace BibliotekaGierPlanszowych
+{
+    /// <summary>
+    /// Interaction logic for SignUpWindow.xaml
+    /// </summary>
+    public partial class SignUpWindow : Window
+    {
+        public SignUpWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+
+        }
+    }
+}
---
+++ BibliotekaGierPlanszowych/UserControlLogin.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BibliotekaGierPlanszowych
+{
+    /// <summary>
+    /// Interaction logic for UserControlLogin.xaml
+    /// </summary>
+    public partial class UserControlLogin : UserControl
+    {
+        public UserControlLogin()
+        {
+            InitializeComponent();
+        }
+    }
+}
