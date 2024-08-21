76a9eb280ca34b9efb1f2dd8ae553a1685604e4a
Tomasz Mozgwa <t.mozgwa@sente.pl>
2024-08-21 13:42:02 +0200

    Metody dla logowania i rejestracji

--- BibliotekaGierPlanszowych/SignUpWindow.xaml.cs
+++ BibliotekaGierPlanszowych/SignUpWindow.xaml.cs
@@ -1,7 +1,8 @@
 ﻿using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
-
+using System;
+using System.Collections.Generic;
 
 namespace BibliotekaGierPlanszowych
 {
@@ -15,10 +16,104 @@ namespace BibliotekaGierPlanszowych
             InitializeComponent();
         }
 
+        private DBConnection db = new DBConnection();
+
         private void Border_MouseDown(object sender, MouseButtonEventArgs e)
         {
             this.DragMove();
+        }
 
+        private void Button_Register_Click(object sender, RoutedEventArgs e)
+        {
+            bool LoginOk = true;
+            if(TextBox_Pass.Password != TextBox_RepPass.Password)
+            {
+                MessageBox.Show("Pola: Hasło i Powtórz hasło muszą być takie same", "Błąd", MessageBoxButton.OK, MessageBoxImage.Information);
+                LoginOk = false;
+            }
+
+            //sprawdzamy czy nie mamy juz takiego loginu
+            List<String> usersList= new List<string>();
+            String queryUsers = "SELECT login FROM users";
+            bool niepoprawnyLogin = false;
+            usersList = db.DatabasQueryExecute(queryUsers);
+          
+            foreach (String item in usersList)
+            {
+                if (item.Equals(this.TextBox_Login.Text))
+                {
+                    niepoprawnyLogin = true;
+                }
+            }
+            if (niepoprawnyLogin)
+            {
+                MessageBox.Show("W bazie istnieje już użytkownik o tym loginie", "Błąd", MessageBoxButton.OK, MessageBoxImage.Information);
+                LoginOk = false;
+            }
+
+
+            if (LoginOk)
+            {
+                String Query = "INSERT INTO users (login, password) VALUES ('" + this.TextBox_Login.Text + "', '" + this.TextBox_Pass.Password + "')";
+
+                try
+                {
+                    db.DatabaseDataChange(Query);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Błąd", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+
+                MessageBox.Show("Zapisano dane logowania", "Informacja", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void Button_Login_Click(object sender, RoutedEventArgs e)
+        {
+            //sprawdzamy czy hasło jest poprawne            
+            String queryLogin = "SELECT password FROM users where login = " +"'"+this.TextBox_Login.Text+"'";
+            List<String> loginList = new List<string>();
+            loginList = db.DatabasQueryExecute(queryLogin);
+            bool logIn = true;
+            bool wrongPass = true;
+            bool noLogin = true;
+
+            foreach (String item in loginList)
+            {
+                noLogin = false;
+                if (item.Equals(this.TextBox_Pass.Password))
+                {
+                    wrongPass = false;
+                }
+            }
+            if (wrongPass && !noLogin)
+            {
+                MessageBox.Show("Niepoprawne hasło", "Błąd", MessageBoxButton.OK, MessageBoxImage.Information);
+                logIn = false;
+            }
+            if (wrongPass && noLogin)
+            {
+                MessageBox.Show("Nie znaleziono użytkownika", "Błąd", MessageBoxButton.OK, MessageBoxImage.Information);
+                logIn = false;
+            }
+
+            if (logIn)
+            {
+                MainWindow main = new MainWindow();
+                main.Show();
+                this.Close();
+            }
+
+
+        }
+        private void ButtonExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void ButtonMin_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
         }
     }
 }
